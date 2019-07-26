fe3fb48e5297dc9cebde828c32a39618764231be
pitaz <peter.odekwo@gmail.com>
2019-07-26 22:47:50 +0100

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCgmKs60eyTJ5-NSq32tcMeRT7HjoaAd3M',
+      authDomain: 'authentication-c6904.firebaseapp.com',
+      databaseURL: 'https://authentication-c6904.firebaseio.com',
+      projectId: 'authentication-c6904',
+      storageBucket: '',
+      messagingSenderId: '1040918185841',
+      appId: '1:1040918185841:web:1a981e1ac8f9000d'
+    });
+  }
+
   render() {
     return (
       <View>
